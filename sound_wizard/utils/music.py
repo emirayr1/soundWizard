9438343d95b4452be57ce60c7d7478437aae18c1
emirayr1 <230101004@mgu.edu.tr>
2025-12-07 01:33:51 +0300

    first commit for music.py

---
+++ sound_wizard/utils/music.py
@@ -0,0 +1,3 @@
+# add music cent stuff and mathematical calculations for music theory
+# different systems, scales, modes, contourpoıint, intervals, chords, chord progressions, voice leading, harmonic analysis, form analysis, rhythmic analysis, timbral analysis
+
